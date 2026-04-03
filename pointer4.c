2ceff38c7459066e10dbf90e834769048feb28cf
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-03 17:42:43 +0900

    pointer4 주석 추가

--- pointer4.c
+++ pointer4.c
@@ -6,5 +6,5 @@ main() {
     printf("%s\n", p + 3); // EA
     printf("%s\n", *p); // K
     printf("%s\n", *(p + 3)); // E
-    printf("%s\n", *p + 2); // R
+    printf("%s\n", *p + 2); // R 틀렸음, *p는 p[0]을 바라보니까 K, 여기서 두칸 이동하니까 L, M 답은 M
 }
