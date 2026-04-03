2094f606ae312c9406fc44e8e807ff991f27f458
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-03 17:41:54 +0900

    pointer4 answer

--- pointer4.c
+++ pointer4.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+main() {
+    char* p = "KOREA";
+    printf("%s\n", p); // KOREA
+    printf("%s\n", p + 3); // EA
+    printf("%s\n", *p); // K
+    printf("%s\n", *(p + 3)); // E
+    printf("%s\n", *p + 2); // R
+}
