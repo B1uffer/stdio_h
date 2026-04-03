9058fbe67539c86c8e9b7ba0eaac03ab84a9799e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-03 15:38:36 +0900

    pointer1 answer

--- pointer1.c
+++ pointer1.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 
 int main() {
+        int a[4] = {0,2,4,8};
+        int b[3]; // b = {2, 2, 4}
+        int* p;
+        int sum = 0;
+
+        for(int i=1; i<4; i++) { // 1,2,3
+                p = a + i; // a[1] - 2, a[2] - 4, a[3] - 8
+                b[i - 1] = *p - a[i - 1]; // b[0] = *p(a[1]) - a[0] = 2
+                sum = sum + b[i - 1] + a[i]; // sum = sum + b[0] + a[1]
+        }
+        printf("%d", sum);
 }
