051af461de9b82b2f2176ecf21f489c495be8b39
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-03 17:08:52 +0900

    pointer2 answer

--- pointer2.c
+++ pointer2.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+int main() {
+    int* array[3]; // {12, 24, 36}
+    int a = 12, b = 24, c = 36;
+    array[0] = &a; // 12
+    array[1] = &b; // 24
+    array[2] = &c; // 36
+    printf("%d", *array[1] + **array + 1); // 24 + 12 + 1 = 37
+}
