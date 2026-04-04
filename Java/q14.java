7851c4d7f8cb0b555c33c805bf117daa2810c7a2
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-05 00:04:07 +0900

    answer q14

--- Java/q14.java
+++ Java/q14.java
@@ -2,6 +2,11 @@ package Java;
 
 public class q14 {
    public static void main(String[] args) {
-      
+      int aa[][] = {{45, 50, 75}, {89}};
+      System.out.println(aa[0].length); // 3
+      System.out.println(aa[1].length); // 1
+      System.out.println(aa[0][0]); // 45
+      System.out.println(aa[0][1]); // 50
+      System.out.println(aa[1][0]); // 89
    }
 }
