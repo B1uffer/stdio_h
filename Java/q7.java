992aea8aea6cd7459463c6324821608942d23f49
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-04 18:14:23 +0900

    q7 answer

--- Java/q7.java
+++ Java/q7.java
@@ -2,6 +2,16 @@ package Java;
 
 public class q7 {
    public static void main(String[] args) {
-      
+      int j, i;
+      for(j = 0, i = 0; i <= 5; i++) {
+         j += i; // 0, 1, 2, 3, 4, 5
+         System.out.println(i); // 0 + 1 + 2 + 3 + 4 5 = 15
+         if(i == 5) {
+            System.out.println("=");
+            System.out.println(j);
+         } else {
+            System.out.println("+");
+         }
+      }
    }
 }
