116545d6f6f7b9e8e3741042d3e97f076013c903
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-05 00:43:09 +0900

    answer class01

--- Java/class01.java
+++ Java/class01.java
@@ -2,6 +2,24 @@ package Java;
 
 public class class01 {
    public static void main(String[] args) {
-      
+      cond obj = new cond(3);
+      obj.a = 5;
+      int b = obj.func(); // 56
+      System.out.println(obj.a + b); // 5 + 56 = 61
+   }
+}
+
+class cond {
+   int a; // 5
+   public cond(int a) {
+      this.a = a;
+   }
+
+   public int func() {
+      int b = 1;
+      for(int i=1; i<a; i++) { // 1부터 4까지
+         b += a * i; // 1 + 5 + 10 + 15 + 20 = 51
+      }
+      return a + b; // 56
    }
 }
