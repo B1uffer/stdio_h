6a1f85ba132fd8691073c469b3e2d912fd1692e9
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-04 22:14:29 +0900

    q10 수정

--- Java/q10.java
+++ Java/q10.java
@@ -3,7 +3,6 @@ package Java;
 public class q10 {
    public static void main(String[] args) {
       int ary[][] = new int[3][5];
-      int n = 1;
       for(int i=0; i<3; i++) { // ary[].length 까지
          for(int j=0; j<5; j++) { // ary[][].length 까지
             ary[i][j] = j * 3 + i + 1;
