968d213a32a34977bf2e2c35696efe03eb27d3e3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-04 22:36:47 +0900

    answer q11

--- Java/q11.java
+++ Java/q11.java
@@ -1,5 +1,12 @@
 package Java;
 
 public class q11 {
-   
+   public static void main(String[] args) {
+      int i = 0, c = 0;
+      while(i < 10) {
+         i++; // 1 2
+         c *= i; // 0 * 1 = 0, 0 * 2 = 0
+      }
+      System.out.println(c); // 0
+   } 
 }
