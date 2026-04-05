58264ada22fcca387c69292986e5c0c21bbd31cc
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-05 17:00:01 +0900

    answer singleton01

---
+++ Java/singleton01.java
@@ -0,0 +1,31 @@
+package Java;
+
+class Connection {
+   private static Connection _inst = null;
+   private int count = 0;
+   static public Connection get() {
+      if(_inst == null) {
+         _inst = new Connection();
+         return _inst;
+      }
+      return _inst;
+   }
+   public void count() {
+      count++;
+   }
+   public int getCount() {
+      return count;
+   }
+}
+
+public class singleton01 {
+   public static void main(String[] args) {
+      Connection conn1 = Connection.get();
+      conn1.count(); // 1
+      Connection conn2 = Connection.get();
+      conn2.count(); // 2
+      Connection conn3 = Connection.get();
+      conn3.count(); // 3
+      System.out.println(conn1.getCount()); // 3
+   }
+}
