228553d053ffe96c4f75b7492c1129ff3b9901e3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-05 17:17:56 +0900

    answer change01

---
+++ Java/change01.java
@@ -0,0 +1,20 @@
+package Java;
+
+class Parent {
+   void show() {
+      System.out.println("parent");
+   }
+}
+
+class Child extends Parent {
+   void show() {
+      System.out.println("child");
+   }
+}
+
+public class change01 {
+   public static void main(String[] args) {
+      Parent pa = new Child();
+      pa.show();
+   }
+}
