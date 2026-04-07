9882975ff10a57eba69fcd4c3a3b7d486bb7062e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-07 16:13:14 +0900

    answer q1

--- C/structure/q1.c
+++ C/structure/q1.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+struct A {
+   int n;
+   int g;
+};
+
+int main() {
+   struct A st[2]; // {{n,g}, {n,g}}
+   for(int i=0; i<2; i++) {
+      st[i].n = i; // 0 , 1
+      st[i].g = i+1; // 1, 2
+   }
+   printf("%d", st[0].n + st[1].g); // 0 + 2 = 2
+}
