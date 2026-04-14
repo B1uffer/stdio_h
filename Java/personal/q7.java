ad7d847a365a89ad737c6c87eca824e64a1c0a79
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-04-15 01:38:48 +0900

    answer q7

--- Java/personal/q7.java
+++ Java/personal/q7.java
@@ -1,5 +1,19 @@
 package Java.personal;
 
 public class q7 {
-   
+   static int[] arr() {
+      int a[] = new int[4]; // 배열 4개
+      int b = a.length; // 4
+      for(int i=0; i<b; i++) { // 0 ~ 3
+         a[i] = i; // 0,1,2,3
+      }
+      return a;
+   }  
+
+   public static void main(String[] args) {
+      int a[] = arr(); // 0,1,2,3
+      for(int i=0; i<a.length; i++) { // 0 ~ 3
+         System.out.println(a[i] + " "); // 0 1 2 3
+      }
+   }
 }
